0f66d33d3ed2baef1b1ff2504f94cd506660bc28
Sundaram Sharma <sundaram-sharma@hotmail.com>
2022-06-21 22:42:28 -0400

    .Added Contants.kt with Needed contants throught the program .Added Toast.kt that will get the required Toast without declaring again and again

---
+++ app/src/main/java/com/example/studenthood/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.studenthood.util
+
+object Constants {
+    const val COLLECTION_NAME_USERS = "users" // constant variable for the  users
+    const val COLLECTION_NAME_POSTS = "posts" // constant variable for the  posts
+    const val REGEX_STRONG_PASSWORD = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})" //constant to validate the passworod
+}
